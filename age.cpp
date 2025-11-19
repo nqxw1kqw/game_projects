af7cd75b3a3a75c33669ef24b811422b88fb388f
mitukiiiqwq <141080440+oromitsuki@users.noreply.github.com>
2025-11-19 10:26:32 +0900

    age.cpp

---
+++ age.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, C++!" << endl;
+    int age = 0;
+    cout << "Enter your age: ";
+    cin >> age;
+    cout << "You are " << age << " years old!" << endl;
+    return 0; 
+}
